b0345c68038af3dae08ac47627237bbfb8dbf2ca
Gabriel Coelho <gabrielroberto099@gmail.com>
2023-09-15 16:25:14 -0400

    v0.1.2 --DROP AND GRAB IMPL

--- Plat0.gd
+++ Plat0.gd
@@ -1,2 +1,7 @@
 extends Area2D
 
+@onready var dropzone = get_node("DropZone")
+var drop_position
+
+func _ready():
+	drop_position = dropzone.global_position
--- Scripts/box_1.gd
+++ Scripts/box_1.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var picked = false
+var dropping = false
 @onready var player = get_node("../Player")
 @onready var label = get_node("Label")
 var drop_position
@@ -8,12 +9,11 @@ var drop_position
 func _physics_process(_delta):
 	if picked:
 		self.position = player.marker.global_position
-		
-	if Input.is_action_just_pressed("ui_drop") and picked == true:
-		picked = false
-		player.canPick = true
-		self.global_position = lerp(self.global_position, drop_position, 10 * _delta)
-		self.rotation = lerp_angle(rotation, 0, 10 * _delta)
+		self.freeze = true
+	if dropping:
+		self.position = drop_position
+		dropping = false
+		self.freeze = false
 
 func _input(_event):
 	var bodies = $Area2D.get_overlapping_bodies()
@@ -22,16 +22,11 @@ func _input(_event):
 			if body is Player and player.canPick == true:
 				picked = true
 				player.canPick = false
-	
-		#picked = false
-		#print(player.sprite.flip_h)
-		#player.canPick = true
+	if Input.is_action_just_pressed("ui_drop") and picked == true:
+		picked = false
+		dropping = true
+		player.canPick = true
 
-func _on_area_2d_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
-	print(body)
-
-func _on_area_2d_area_shape_entered(_area_rid, area, _area_shape_index, _local_shape_index):
-	#get postion of dropzone on overlap
-	print(area.is_in_group("Platform"))
-	drop_position = area.get_node("DropZone").global_position
-	print(drop_position)
+func _on_area_2d_area_entered(area):
+	#get drop_pos by marker of platform
+	drop_position = area.drop_position
