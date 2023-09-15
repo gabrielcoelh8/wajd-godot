2174b2966ce4131c9d5409ed8eb8be0c37640de6
Gabriel Coelho <gabrielroberto099@gmail.com>
2023-09-15 17:00:04 -0400

    v0.1.2 --RANDOM NUMBERS IMPL

--- world.gd
+++ world.gd
@@ -1,42 +1,44 @@
 extends Node2D
 @onready var Box1Label = get_node("Box1/Label")
-@onready var numbers = [1, 2, 3, 4, 5]
-@onready var lastnumber
+@onready var boxes_nodes = get_tree().get_nodes_in_group("boxes")
+@onready var numbers = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var boxes_numbers = []
-	var boxes_nodes = get_tree().get_nodes_in_group("boxes")
 	
 	for i in range(boxes_nodes.size()):
-		boxes_numbers.append(get_random_number())
-		boxes_nodes[i].label.set_text(str(boxes_numbers[i]))
-		
-	#boxes_numbers.sort_custom(bubblesort)
-	print(boxes_numbers)
-	#Box1Label.set_text(str(boxes_numbers[0]))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+		var unique_number = generate_unique_random()
+		numbers.append(unique_number)
+		boxes_nodes[i].label.set_text(str(unique_number))
+	
+	print(numbers)
 
 func bubblesort(a, b):
 	if a[0] < b[0]:
 		return true
 	return false
 	
-func get_random_number():
-	var random_number = numbers[randi() % numbers.size()]
-	while random_number == lastnumber:
-		# The last fruit was picked, try again until we get a different fruit.
-		random_number = numbers[randi() % numbers.size()]
+	
+func bubble_sort(arr):
+	var n = arr.size()
+	var swapped = false
+	
+	for i in range(n-1):
+		swapped = false
+		for j in range(n-1-i):
+			if arr[j] > arr[j+1]:
+				arr.swap(j, j+1)
+				swapped = true
+		if not swapped:
+			break
 
-	# Note: if the random element to pick is passed by reference,
-	# such as an array or dictionary,
-	# use `_last_fruit = random_fruit.duplicate()` instead.
-	lastnumber = random_number
-
-	# Returns "apple", "orange", "pear", or "banana" every time the code runs.
-	# The function will never return the same fruit more than once in a row.
+var used_numbers : Array = []
+func generate_unique_random():
+	var random_number = -1
+	while true:
+		random_number = randi_range(1, 5)
+		if used_numbers.find(random_number) == -1:
+			used_numbers.append(random_number)
+			break
 	return random_number
