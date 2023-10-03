dddce1c3f0e3d5c77a3b8093d0d18421d98bdb08
Gabriel Coelho <gabrielroberto099@gmail.com>
2023-10-03 17:00:10 -0400

    v0.3.1 ALPHA -menu impl

---
+++ Scripts/Menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _on_tutorial_pressed():
+	pass # Replace with function body.
+
+
+func _on_normal_pressed():
+	pass # Replace with function body.
+
+
+func _on_teoria_pressed():
+	pass # Replace with function body.
+
+
+func _on_sair_pressed():
+	pass # Replace with function body.
